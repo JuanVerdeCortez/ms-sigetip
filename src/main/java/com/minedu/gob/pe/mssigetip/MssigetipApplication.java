1938e693519d7f6d4fbbe41fc2bc1fb58114771e
unknown <juanverdec@gmail.com>
2024-10-26 01:27:50 -0500

    feature: first commit

---
+++ src/main/java/com/minedu/gob/pe/mssigetip/MssigetipApplication.java
@@ -0,0 +1,13 @@
+package com.minedu.gob.pe.mssigetip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssigetipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssigetipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minedu/gob/pe/mssigetip/MssigetipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minedu.gob.pe.mssigetip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssigetipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
